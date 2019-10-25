6aedb55dea2372c7d600a0487a37caf0c9c0a9ca
Akramurridjal Rahman <xerod@outlook.com>
2019-10-26 00:44:09 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Open Sans', '-apple-system', 'BlinkMacSystemFont'],
+      'serif': ['Georgia', 'Cambria'],
+      'mono': ['SFMono-Regular', 'Menlo'],
+    },
+  },
+  variants: {},
+  plugins: []
+}
